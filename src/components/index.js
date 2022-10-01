771b1be6a48fba41a8e101b46fdc235cfb8630f3
RitikParikh <ritik.parikh@gig4ce.com>
2022-10-01 13:04:16 +0530

    TUT:45

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+// import logo from '../logo.svg';
+import '../App.css';
+import ClassCounter from './ClassCounter';
+import HookCounter from './HookCounter';
+
+function Basic() {
+    return (
+      <div className='App'>
+        <ClassCounter/> 
+        <HookCounter/> 
+      </div>
+    );
+  }
+export default Basic;
