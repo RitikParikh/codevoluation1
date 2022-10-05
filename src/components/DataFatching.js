982e77aef9ac1968470f3bc2d6086c7a6335b77c
RitikParikh <ritik.parikh@gig4ce.com>
2022-10-05 21:05:16 +0530

    TUT:55

--- src/components/DataFatching.js
+++ src/components/DataFatching.js
@@ -2,22 +2,33 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 
 function DataFatching() {
-  const [posts, setPosts]= useState([]);
+  // const [posts, setPosts]= useState({});
+  const [post, setPost]= useState({});
+  const [id, setId]= useState(1);
+  const [idFromButtonClick, setIdFromButtonClick]= useState(1);
   useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(({data}) =>{
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`).then(({data}) =>{
         console.log(data);
-        setPosts(data);
+        setPost(data);
+        // setPosts(data);
+        // setPosts(data);
     }).catch(err =>{
         console.log(err);
     });
-  },[]);
+  },[idFromButtonClick]);
+
     return (
     <div>
-        {
+      <input type="text" value={id} onChange={e=> setId(e.target.value)}/>
+      <div>{post.title}</div>
+      <button type="submit" onClick={()=>setIdFromButtonClick(id)}>FatchPost</button>
+      <ul>
+        {/* {
             posts.map(post => 
                  <li key={post.id}>{post.title}</li>
             )
-        }
+        } */}
+      </ul>
     </div>
   )
 }
